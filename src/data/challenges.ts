bcc742ad1c8e33ccdc90daaf661f54f053250526
claytonfortunato <claytonreis10@gmail.com>
2023-04-21 23:52:37 -0300

    feat: data info challenges

---
+++ src/data/challenges.ts
@@ -0,0 +1,19 @@
+export const challenges = [
+  {
+    id: 1,
+    img: "src/assets/img/Music-player.png",
+    title: "Desafio #01 - Music Player",
+    description: "Uma page com a face de music player.",
+    site: "https://reliable-unicorn-2ccf84.netlify.app/",
+    github: "https://github.com/claytonfortunato/Music-player-01",
+  },
+  {
+    id: 2,
+    img: "src/assets/img/product-card.png",
+    title: "Desafio #02 - Produto Card",
+    description:
+      "Um card de produto que possui a função de 360 grau para mostrar o card.",
+    site: "https://fancy-gecko-c54685.netlify.app/",
+    github: "https://github.com/claytonfortunato/ProductCard-02",
+  },
+];
